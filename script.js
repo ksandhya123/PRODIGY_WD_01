62025dcdbb7344f8fb58f0e588f83dfc7150e392
ksandhya123 <maheswari.karibandi@gmail.com>
2024-07-06 22:38:11 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add event listener for scroll event
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
